306d1860724fd9c0aa31d8795c5220ee4a74c126
Nutt Chairatana <nuttchai@outlook.com>
2023-09-08 04:07:37 +0700

    export constant

---
+++ constant/columns.py
@@ -0,0 +1,22 @@
+COLS = [
+    "time",
+    "timestamp",
+    "cpu_usage",
+    "memory_usage",
+    "bandwidth_inbound",
+    "bandwidth_outbound",
+    "tps",
+    "response_time",
+    "status"
+]
+
+FEATURES = [
+    "cpu_usage",
+    "memory_usage",
+    "bandwidth_inbound",
+    "bandwidth_outbound",
+    "response_time",
+    "tps"
+]
+
+LABEL = "status"
---
+++ constant/path.py
@@ -0,0 +1,5 @@
+BASE = '../../rcie-system-metrics-data/'
+DATE = '2023-06-12_11-33-10_rcie-api-default/'
+FILE = 'metrics-with-output-and-standardized-unit.csv'
+
+PATH = BASE + DATE + FILE
