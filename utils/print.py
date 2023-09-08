712e0c6de5abfdd11ad44064a0a13d2ef14dc2c4
Nutt Chairatana <nuttchai@outlook.com>
2023-09-08 13:05:15 +0700

    add utils

--- utils/print.py
+++ utils/print.py
@@ -1,4 +1,5 @@
 from numpy import mean, std
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 def print_cross_validation_scores(scores):
     print('MACRO:')
@@ -10,3 +11,9 @@ def print_cross_validation_scores(scores):
     print('Precision: %.4f (%.4f)' % (mean(scores['test_precision_micro']), std(scores['test_precision_micro'])))
     print('Recall: %.4f (%.4f)' % (mean(scores['test_recall_micro']), std(scores['test_recall_micro'])))
     print('F1 score: %.4f (%.4f)' % (mean(scores['test_f1_micro']), std(scores['test_f1_micro'])))
+
+def print_scores(y_test, y_pred):
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Precision:", precision_score(y_test, y_pred))
+    print("Recall:", recall_score(y_test, y_pred))
+    print("F1:", f1_score(y_test, y_pred))
