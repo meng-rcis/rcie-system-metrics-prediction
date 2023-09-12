88b6b31954021928dbb600bad3d960efb4e2069b
Nutt Chairatana <nuttchai@outlook.com>
2023-09-13 02:24:01 +0700

    add CSV headers

---
+++ models/features/prediction/constant/models_header.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+# Add path to the root folder
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+import constant.models_id as models_id
+
+BASE_HEADERS = [ 
+    models_id.ARIMA,
+    models_id.LSTM,
+    models_id.CNN,
+    models_id.PROPHET,
+    models_id.ETS,
+]
+
+META_HEADERS = [
+    models_id.REGRESSION_STACK,
+    models_id.TREE_STACK,
+    models_id.NEURAL_STACK,
+]
