042fadc919acfca78b9af9b9564752af1c5d4521
Nutt Chairatana <nuttchai@outlook.com>
2023-09-13 02:23:51 +0700

    update data manager to load dataset and write and read CSV

--- models/features/prediction/manager/data_manager.py
+++ models/features/prediction/manager/data_manager.py
@@ -1,4 +1,34 @@
+import os
+import sys
+import csv
+
+# Add path to the root folder
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+import utils.load as load
+import pandas as pd
+
 class DataManager:
-    def __init__(self, csv_file: str):
-        self.csv_file = csv_file
+    def __init__(self):
+        pass
+    
+    def LoadDataset(self, filename: str):
+        return load(filename)
+    
+    def WriteCSV(self, filename: str, header: list[str], rows: list[list[str]]):
+        # Check if file exists and has content
+        file_exists = os.path.exists(filename)
+        
+        with open(filename, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+
+            # If file doesn't exist or is empty, write the header
+            if not file_exists or os.path.getsize(filename) == 0:
+                writer.writerow(header)
+
+            # Write the data rows
+            writer.writerows(rows)
+
+    def ReadCSV(self, filename: str):
+        return pd.read_csv(filename)
+    
     
