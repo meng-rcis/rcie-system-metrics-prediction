2d2a714034bb81c7811e721afe713d4b23061918
Nutt Chairatana <nuttchai@outlook.com>
2023-09-13 02:24:20 +0700

    initial setup

--- models/features/prediction/main.py
+++ models/features/prediction/main.py
@@ -3,6 +3,7 @@ import sys
 
 # Add path to the root folder
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+from manager.data_manager import DataManager
 from manager.setup_manager import SetupManager
 from manager.main_manager import MainManager
 
@@ -11,7 +12,11 @@ def main():
     isSetupMetaModelDatasetRequired = False
     if isSetupMetaModelDatasetRequired:
         print("Preparing meta model dataset...")
-        SetupManagerInstance = SetupManager()
+        DataManagerInstance = DataManager()
+        SetupManagerInstance = SetupManager(
+            meta_source='../source/training/meta.csv',
+            data_source=DataManagerInstance.LoadDataset('../../../dump/df.p'),
+            )
         SetupManagerInstance.PrepareMetaModelDataset()
 
     # Start Main Process
