2911ef4a7935b0de1e7754965bc8b0bde1be5078
Nutt Chairatana <nuttchai@outlook.com>
2023-09-14 04:15:16 +0700

    add skeleton code

---
+++ models/features/prediction/infrastructure/L1_base_models/prophet.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+# Add path to the root folder
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+from interface.model import IModel
+
+class Prophet( IModel ):
+    def __init__(self):
+        pass
+
+    def TrainModel(self, input):
+        pass
+
+    def TuneModel(self, input):
+        pass
+
+    def Predict(self, input):
+        pass
