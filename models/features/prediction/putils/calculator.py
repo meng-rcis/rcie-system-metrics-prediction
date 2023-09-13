5a53894a2ff6943e86d65cb69b644a8e571bd850
Nutt Chairatana <nuttchai@outlook.com>
2023-09-14 04:15:07 +0700

    add initial weight

--- models/features/prediction/putils/calculator.py
+++ models/features/prediction/putils/calculator.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 class Calculator:
-    def __init__(self, config):
-        self.config = config
+    def __init__(self, initial_weight=1):
+        self.initial_weight = initial_weight
 
     def CalculateWeight(self, data: pd.DataFrame):
         raise NotImplementedError
