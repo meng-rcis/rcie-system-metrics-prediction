7cb6469d25431cacece2d318e19a22b05967ed66
Nutt Chairatana <nuttchai@outlook.com>
2023-09-15 03:21:10 +0700

    update abstract model class

--- models/features/prediction/interface/model.py
+++ models/features/prediction/interface/model.py
@@ -1,16 +1,21 @@
 import abc
+import pandas as pd
 
 class IModel( abc.ABC ):
     @abc.abstractmethod
-    def TrainModel(self, input: Any) -> Any:
+    def AssignDataset(self, dataset: pd.DataFrame):
         pass
 
     @abc.abstractmethod
-    def TuneModel(self, input: Any) -> Any:
+    def TrainModel(self, input: any) -> any:
+        pass
+
+    @abc.abstractmethod
+    def TuneModel(self, input: any) -> any:
         pass 
 
     @abc.abstractmethod
-    def Predict(self, input: Any) -> Any:
+    def Predict(self, input: any) -> pd.DataFrame:
         pass
 
 
