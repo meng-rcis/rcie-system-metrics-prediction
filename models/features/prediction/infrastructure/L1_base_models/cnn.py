e85c44de7a2c5055dea74ac36da65bfe880eaeae
Nutt Chairatana <nuttchai@outlook.com>
2023-09-21 03:26:53 +0700

    prep CNN

---
+++ models/features/prediction/infrastructure/L1_base_models/cnn.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+# Add path to the root folder
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+from interface.model import IModel
+import pandas as pd
+
+class CNN( IModel ):
+    def __init__(self):
+        self.dataset = None
+        self.model = None
+    
+    def AssignDataset(self, dataset: pd.DataFrame, feature: str):
+        self.dataset = dataset[feature]
+
+    def TrainModel(self, config):
+        pass
+
+    def TuneModel(self, config):
+        pass
+
+    def Predict(self, config):
+        prediction = self.model.predict(x=config['x'])
