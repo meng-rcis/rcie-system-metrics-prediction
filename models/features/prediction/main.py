6b724b4959e3389ee2027d3d5f614e39fa352f31
Nutt Chairatana <nuttchai@outlook.com>
2023-10-31 03:36:22 +0700

    initial main process

--- models/features/prediction/main.py
+++ models/features/prediction/main.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import math
 
 # Add path to the root folder
 sys.path.append(
@@ -13,18 +12,20 @@ from manager.setup_manager import SetupManager
 from manager.main_manager import MainManager
 from config.control import (
     BASE_MODELS_IDS,
+    META_MODELS_IDS,
     SELECTED_FEATURE,
     PREDICTION_STEPS,
     INITIAL_BASE_TRAINING_SIZE,
     INITIAL_META_TRAINING_SIZE,
-    INITIAL_BASE_TRAINING_SIZE_META,
+    INITIAL_BASE_TRAINING_SIZE_IN_MAIN,
+    INITIAL_META_TRAINING_SIZE_IN_MAIN,
     IS_FILTERED,
     IS_SETUP_META_MODEL_DATASET_REQUIRED,
 )
 from config.path import (
     L1_META_DATASET_PATH,
     L2_META_DATASET_PATH,
-    META_ARCHIVE_DIRECTORY,
+    FINAL_PREDICTION_PATH,
     BASE_DATASET_PATH,
 )
 
@@ -38,7 +39,6 @@ def main():
             dataset=DataManagerInstance.LoadDataset(BASE_DATASET_PATH),
             selected_feature=SELECTED_FEATURE,
             meta_training_path=L1_META_DATASET_PATH,
-            meta_archive_directory=META_ARCHIVE_DIRECTORY,
             base_model_ids=BASE_MODELS_IDS,
             prediction_steps=PREDICTION_STEPS,
             initial_base_training_size=INITIAL_BASE_TRAINING_SIZE,
@@ -50,10 +50,17 @@ def main():
     # Start Main Process
     print("Starting main process...")
     MainManagerInstance = MainManager(
+        base_dataset=DataManagerInstance.LoadDataset(BASE_DATASET_PATH),
+        selected_feature=SELECTED_FEATURE,
         l1_meta_training_path=L1_META_DATASET_PATH,
         l2_meta_training_path=L2_META_DATASET_PATH,
-        initial_base_training_size=INITIAL_BASE_TRAINING_SIZE_META,
+        final_prediction_path=FINAL_PREDICTION_PATH,
+        base_model_ids=BASE_MODELS_IDS,
+        meta_model_ids=META_MODELS_IDS,
+        initial_base_training_size=INITIAL_BASE_TRAINING_SIZE_IN_MAIN,
+        initial_meta_training_size=INITIAL_META_TRAINING_SIZE_IN_MAIN,
         prediction_steps=PREDICTION_STEPS,
+        is_filtered=IS_FILTERED,
     )
     MainManagerInstance.Run()
 
