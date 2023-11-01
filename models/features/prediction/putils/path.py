37cb871b89f4ef24373955f0cbe53ffb38dcba9e
Nutt Chairatana <nuttchai@outlook.com>
2023-11-02 01:35:55 +0700

    update function to generate archive directory path

--- models/features/prediction/putils/path.py
+++ models/features/prediction/putils/path.py
@@ -1,20 +1,12 @@
 import time
 
 
-def generate_meta_archive_directory(layer: str) -> str:
-    if layer != "l1" and layer != "l2" and layer != "final":
+def generate_meta_archive_directory_path(
+    layer: str, folder_name: str = str(round(time.time() * 1000))
+) -> str:
+    if layer != "l1" and layer != "l2" and layer != "l3":
         raise Exception("Invalid layer")
-    if layer == "final":
-        path = (
-            "models/features/source/"
-            + "l3_prediction_dataset/archive/"
-            + str(round(time.time() * 1000))
-        )
-        return path
     path = (
-        "models/features/source/"
-        + layer
-        + "_prediction_dataset/archive/"
-        + str(round(time.time() * 1000))
+        "models/features/source/" + layer + "_prediction_dataset/archive/" + folder_name
     )
     return path
