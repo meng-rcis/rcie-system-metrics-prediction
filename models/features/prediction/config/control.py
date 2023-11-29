bd5dd06dab1b30370f3bb60beede4a116dc0994f
Nutt Chairatana <nuttchai@outlook.com>
2023-11-30 00:26:50 +0700

    prepare config for tmr training (inbound)

--- models/features/prediction/config/control.py
+++ models/features/prediction/config/control.py
@@ -38,7 +38,7 @@ META_MODELS_IDS - define the list of meta model ids here
 
 CONFIG = {
     # SELECTED_FEATURE's options: "cpu_usage", "memory_usage", "bandwidth_inbound", "bandwidth_outbound", "tps", "response_time"
-    "SELECTED_FEATURE": "memory_usage",
+    "SELECTED_FEATURE": "bandwidth_inbound",
     "START_TRAINING_INDEX": 0,
     "PREDICTION_STEPS": 5,
     "INITIAL_BASE_TRAINING_SIZE": 1000,
