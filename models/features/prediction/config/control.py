7f3316466ce04224af526852260716506b479889
Nutt Chairatana <nuttchai@outlook.com>
2023-11-29 14:31:47 +0700

    MEMORY_USAGE -> 1250 Size

--- models/features/prediction/config/control.py
+++ models/features/prediction/config/control.py
@@ -37,7 +37,7 @@ META_MODELS_IDS - define the list of meta model ids here
 
 CONFIG = {
     # SELECTED_FEATURE's options: "cpu_usage", "memory_usage", "bandwidth_inbound", "bandwidth_outbound", "tps", "response_time"
-    "SELECTED_FEATURE": "cpu_usage",
+    "SELECTED_FEATURE": "memory_usage",
     "START_TRAINING_INDEX": 0,
     "PREDICTION_STEPS": 5,
     "INITIAL_BASE_TRAINING_SIZE": 1000,
