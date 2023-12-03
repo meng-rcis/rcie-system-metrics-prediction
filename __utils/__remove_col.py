e68e4be216667b9d5c859a6608dd966748797c95
Nutt Chairatana <nuttchai@outlook.com>
2023-12-03 16:48:02 +0700

    update file to remove col

--- __utils/__remove_col.py
+++ __utils/__remove_col.py
@@ -1,26 +1,22 @@
 import pandas as pd
 
 
-def remove_column_from_csv(csv_file_path, column_name):
+def remove_column_from_csv(csv_file_path, column_names):
     df = pd.read_csv(csv_file_path)
-    if column_name in df.columns:
-        df = df.drop(column_name, axis=1)
-        df.to_csv(csv_file_path, index=False)
+    for column_name in column_names:
+        if column_name in df.columns:
+            df = df.drop(column_name, axis=1)
+    df.to_csv(csv_file_path, index=False)
 
 
-def loop_remove_column_from_csv(csv_file_paths, column_name):
+def loop_remove_column_from_csv(csv_file_paths, column_names):
     for csv_file_path in csv_file_paths:
-        remove_column_from_csv(csv_file_path, column_name)
+        remove_column_from_csv(csv_file_path, column_names)
 
 
 loop_remove_column_from_csv(
     [
-        "./models/features/tuning/cpu_usage/source/l1.csv",
-        "./models/features/tuning/memory_usage/source/l1.csv",
-        "./models/features/tuning/inbound_bandwidth/source/l1.csv",
-        "./models/features/tuning/outbound_bandwidth/source/l1.csv",
-        "./models/features/tuning/tps/source/l1.csv",
-        "./models/features/tuning/response_time/source/l1.csv",
+        "./models/features/source/l1_prediction_dataset/prediction_result_filtered.csv",
     ],
-    "GP",
+    ["GP", "ETS"],
 )
