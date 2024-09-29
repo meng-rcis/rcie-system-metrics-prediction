40a1ab9d93e477c0c324c2613f83483f39db5ebb
Nutt Chairatana <nuttchai@outlook.com>
2024-09-30 01:09:35 +0700

    chore: change config to use 8 cpu count instead of max count

--- models/features/prediction/config/os.py
+++ models/features/prediction/config/os.py
@@ -1,4 +1,4 @@
 import os
 
-MAXIMUM_NUMBER_OF_PROCESS = os.cpu_count()
-# MAXIMUM_NUMBER_OF_PROCESS = 8  # Windows OS
+# MAXIMUM_NUMBER_OF_PROCESS = os.cpu_count()
+MAXIMUM_NUMBER_OF_PROCESS = 8  # Windows OS
